179b67d2e4323aad2bd8ad02e064bc8e1e084d7b
Nils Streedain <business@nilsstreedain.com>
2021-02-16 16:28:15 -0800

    Initial Commit
    - Created LargestNumberSolver.java
    - Started required methods
    - Imported required libraries for said methods

---
+++ LargestNumberSolver.java
@@ -0,0 +1,36 @@
+package assign04;
+
+import java.math.BigInteger;
+import java.util.Comparator;
+import java.util.List;
+
+public class LargestNumberSolver {
+	public static <T> void insertionSort(T[] arr, Comparator<? super T> cmp) {
+		
+	}
+	
+	public static BigInteger findLargestNumber(Integer[] arr) {
+		return null;
+	}
+	// throws OutOfRangeException
+	public static int findLargestInt(Integer[] arr) {
+		return 0;
+	}
+	
+	// throws OutOfRangeException
+	public static long findLargestLong(Integer[] arr) {
+		return 0;
+	}
+	
+	public static BigInteger sum(List<Integer[]> list) {
+		return null;
+	}
+	
+	public static Integer[] findKthLargest(List<Integer[]> list, int k) throws IllegalArgumentException {
+		return null;
+	}
+	
+	public static List<Integer[]> readFile(String filename) {
+		return null;
+	}
+}
