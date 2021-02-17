234e661300877d4acfec631759247ff61e37fbe2
GlutenFreeBees <60480751+GlutenFreeBees@users.noreply.github.com>
2021-02-16 18:11:29 -0700

    comments

--- LargestNumberSolver.java
+++ LargestNumberSolver.java
@@ -5,10 +5,20 @@ import java.util.Comparator;
 import java.util.List;
 
 public class LargestNumberSolver {
+	
 	/*
 	 * This generic method sorts the input array using an insertion sort and the
 	 * input Comparator object.
+	 * 
+	 * A priority queue that supports access of the minimum element only.
+	 * 
+	 * @author Kyle Williams and Nils Streedain
+	 * 
+	 * @version February 16, 2021
+	 * 
+	 * @param <T> - the type of elements contained in this priority queue
 	 */
+	
 	public static <T> void insertionSort(T[] arr, Comparator<? super T> cmp) {
 
 	}
@@ -19,6 +29,10 @@ public class LargestNumberSolver {
 	 * number that can be formed is 0. This method must not alter the given array
 	 * and must call your insertionSort method with a Comparator or lambda
 	 * expression that you design.
+	 * 
+	 * @Param arr - an array of Integers
+	 * 
+	 * @Return the largest number in the provided array
 	 */
 	public static BigInteger findLargestNumber(Integer[] arr) {
 		return null;
@@ -31,6 +45,12 @@ public class LargestNumberSolver {
 	 * Logic for solving the problem of determining the largest number should not
 	 * appear again in this method — call an existing public method or a helper
 	 * method. This method must not alter the given array.
+	 * 
+	 * @Param arr - an array of Integers
+	 * 
+	 * @Throws OutOfRangeException if largest number is too large for int
+	 * 
+	 * @Return largest number in data type int
 	 */
 	public static int findLargestInt(Integer[] arr) throws OutOfRangeException {
 		return 0;
@@ -39,6 +59,12 @@ public class LargestNumberSolver {
 	/*
 	 * This method behaves the same as the previous method, but for data type long
 	 * instead of data type int.
+	 * 
+	 * @Param arr - an array of Integers
+	 * 
+	 * @Throws OutOfRangeException if
+	 * 
+	 * @Return largest number in data type long
 	 */
 	public static long findLargestLong(Integer[] arr) throws OutOfRangeException {
 		return 0;
@@ -47,6 +73,10 @@ public class LargestNumberSolver {
 	/*
 	 * This method sums the largest numbers that can be formed by each array in the
 	 * given list. This method must not alter the given list.
+	 * 
+	 * @Param arr - an array of Integers
+	 * 
+	 * @Return - the sum of all elements in the array
 	 */
 	public static BigInteger sum(List<Integer[]> list) {
 		return null;
@@ -61,6 +91,12 @@ public class LargestNumberSolver {
 	 * thrown if k is not a valid position in the list. This method must not alter
 	 * the given list and must call your insertionSort method with a Comparator or
 	 * lambda expression that you design.
+	 * 
+	 * @Param arr - an array of Integers
+	 * 
+	 * @Throws IllegalArgumentException if k is an invalid position in the list
+	 * 
+	 * @Return - the kth largest integer
 	 */
 	public static Integer[] findKthLargest(List<Integer[]> list, int k) throws IllegalArgumentException {
 		return null;
@@ -70,6 +106,10 @@ public class LargestNumberSolver {
 	 * This method generates list of integer arrays from an input file, such that
 	 * each line corresponds to one array of integers separated by blank spaces, and
 	 * returns an empty list if the file does not exist.
+	 * 
+	 * @Param filename - the name of the file to be read
+	 * 
+	 * @Return - an array of integers within the file
 	 */
 	public static List<Integer[]> readFile(String filename) {
 		return null;
